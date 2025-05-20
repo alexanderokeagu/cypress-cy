6e2c3a97fd4698b96176b950c824ceb8b29fec1a
alexanderokeagu <pezokeagu@gmail.com>
2025-05-20 22:25:56 +0100

    test

--- cypress/e2e/automationExercise.cy.ts
+++ cypress/e2e/automationExercise.cy.ts
@@ -1,5 +1,5 @@
 describe('Automation Exercise', ()=> {
     it('should visit the website',{defaultCommandTimeout: 60000}, () => {
-        cy.visit('https://www.automationexercise.com/')
+        cy.visit("https://www.automationexercise.com/");
     })
 })
